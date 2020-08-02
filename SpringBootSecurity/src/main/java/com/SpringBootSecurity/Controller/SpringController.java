0b41edfb3e4e0ce25317a738071229ee767e2b33
rajendra.prasad <rajendra.prasad@BAN11LAP5000554.client.in.r5.madm.net>
2020-08-02 12:31:27 +0530

    First Commit..

---
+++ SpringBootSecurity/src/main/java/com/SpringBootSecurity/Controller/SpringController.java
@@ -0,0 +1,26 @@
+package com.SpringBootSecurity.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringController {
+
+	@GetMapping("/")
+	public String home() {
+		return ("<h1>Welcome</h1>");
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return ("<h1>Welcome user</h1>");
+	}
+
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return ("<h1>Welcome admin</h1>");
+	}
+	
+}
+
